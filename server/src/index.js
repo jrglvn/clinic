c3d8b436dae75642d63fa62958c08c07de18e36b
jrglvn <jrglvn@yahoo.com>
2020-07-13 13:32:35 +0200

    saving

---
+++ server/src/index.js
@@ -0,0 +1,76 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling",
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton",
+  },
+];
+const typeDefs = gql`
+  type Client {
+    id: ID!
+    first_name: String!
+    last_name: String!
+    email: String
+    address: String
+    phone_number: String
+  }
+  type User {
+    id: ID!
+    first_name: String!
+    last_name: String!
+  }
+
+  type Category {
+    id: ID!
+    name: String
+  }
+
+  type Image {
+    id: ID!
+    name: String
+    description: String
+    base64data: String!
+  }
+
+  type Appointment {
+    id: ID!
+    client_id: Int!
+    user_id: Int!
+    category_id: Int!
+    date: String!
+    result: String
+    previous_appointment_id: Int
+  }
+
+  type Query {
+    clients: [Client]!
+    users: [User]!
+    categories: [Category]!
+    images: [Image]!
+    appointments: [Appointment]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    categories: () => [
+      { id: "1", name: "neurologija" },
+      { id: "2", name: "plastična kirurgija" },
+    ],
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
